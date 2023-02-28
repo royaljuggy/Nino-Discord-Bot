df0c3c7669baa5c3990c13cb89ef14d412c1377e
Jacob UW <jmeksava@uwaterloo.ca>
2023-02-28 10:52:03 -0500

    Initial commit and main.js

---
+++ main.js
@@ -0,0 +1,168 @@
+const { REST, Routes } = require('discord.js');
+require("dotenv").config();
+const TOKEN = process.env.TOKEN;
+const PREFIX = '%'
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+    ]
+});
+
+const settings = {
+    prefix: PREFIX,
+    token: TOKEN
+};
+
+const { Player } = require("discord-music-player");
+const player = new Player(client, {
+    leaveOnEmpty: true, // This options are optional.
+    deafenOnJoin: true,
+    volume: 100,
+    quality: "high",
+});
+
+// You can define the Player as *client.player* to easily access it.
+client.player = player;
+
+// Init the event listener only once (at the top of your code).
+client.player
+    // Emitted when channel was empty.
+    .on('channelEmpty',  (queue) =>
+        console.log(`Everyone left the Voice Channel, queue ended.`))
+    // Emitted when a song was added to the queue.
+    .on('songAdd',  (queue, song) =>
+        console.log(`Song ${song} was added to the queue.`))
+    // Emitted when a playlist was added to the queue.
+    .on('playlistAdd',  (queue, playlist) =>
+        console.log(`Playlist ${playlist} with ${playlist.songs.length} was added to the queue.`))
+    // Emitted when there was no more music to play.
+    .on('queueDestroyed',  (queue) =>
+        console.log(`The queue was destroyed.`))
+    // Emitted when the queue was destroyed (either by ending or stopping).    
+    .on('queueEnd',  (queue) =>
+        console.log(`The queue has ended.`))
+    // Emitted when a song changed.
+    .on('songChanged', (queue, newSong, oldSong) =>
+        console.log(`${newSong} is now playing.`))
+    // Emitted when a first song in the queue started playing.
+    .on('songFirst',  (queue, song) =>
+        console.log(`Started playing ${song}.`))
+    // Emitted when someone disconnected the bot from the channel.
+    .on('clientDisconnect', (queue) =>
+        console.log(`I was kicked from the Voice Channel, queue ended.`))
+    // Emitted when deafenOnJoin is true and the bot was undeafened
+    .on('clientUndeafen', (queue) =>
+        console.log(`I got undefeanded.`))
+    // Emitted when there was an error in runtime
+    .on('error', (error, queue) => {
+        console.log(`Error: ${error} in ${queue.guild.name}`);
+    });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const { RepeatMode } = require('discord-music-player');
+
+// music bot
+client.on('messageCreate', async (message) => {
+    if (message.content[0] != PREFIX) return;
+    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+    const command = args.shift();
+    let guildQueue = client.player.getQueue(message.guild.id);
+
+    if(command === 'play') {
+        let queue = client.player.createQueue(message.guild.id);
+        await queue.join(message.member.voice.channel);
+        console.log(args);
+        let song = await queue.play(args.join(' ')).catch(err => {
+            console.log(err);
+            if(!guildQueue)
+                queue.stop();
+        });
+        message.channel.send(`Now playing ${song}`)
+    }
+
+    if(command === 'playlist') {
+        let queue = client.player.createQueue(message.guild.id);
+        await queue.join(message.member.voice.channel);
+        let song = await queue.playlist(args.join(' ')).catch(err => {
+            console.log(err);
+            if(!guildQueue)
+                queue.stop();
+        });
+    }
+
+    if(command === 'skip') {
+        guildQueue.skip();
+    }
+
+    if(command === 'stop') {
+        guildQueue.stop();
+    }
+
+    if(command === 'removeLoop') {
+        guildQueue.setRepeatMode(RepeatMode.DISABLED); // or 0 instead of RepeatMode.DISABLED
+    }
+
+    if(command === 'toggleLoop') {
+        guildQueue.setRepeatMode(RepeatMode.SONG); // or 1 instead of RepeatMode.SONG
+    }
+
+    if(command === 'toggleQueueLoop') {
+        guildQueue.setRepeatMode(RepeatMode.QUEUE); // or 2 instead of RepeatMode.QUEUE
+    }
+
+    if(command === 'setVolume') {
+        guildQueue.setVolume(parseInt(args[0]));
+    }
+
+    if(command === 'seek') {
+        guildQueue.seek(parseInt(args[0]) * 1000);
+    }
+
+    if(command === 'clearQueue') {
+        guildQueue.clearQueue();
+    }
+
+    if(command === 'shuffle') {
+        guildQueue.shuffle();
+    }
+
+    if(command === 'getQueue') {
+        console.log(guildQueue);
+    }
+
+    if(command === 'getVolume') {
+        console.log(guildQueue.volume)
+    }
+
+    if(command === 'nowPlaying') {
+        console.log(`Now playing: ${guildQueue.nowPlaying}`);
+    }
+
+    if(command === 'pause') {
+        guildQueue.setPaused(true);
+    }
+
+    if(command === 'resume') {
+        guildQueue.setPaused(false);
+    }
+
+    if(command === 'remove') {
+        guildQueue.remove(parseInt(args[0]));
+    }
+
+    if(command === 'createProgressBar') {
+        const ProgressBar = guildQueue.createProgressBar();
+        
+        // [======>              ][00:35/2:20]
+        console.log(ProgressBar.prettier);
+    }
+})
+
+// client.login(TOKEN);
+client.login(TOKEN);
